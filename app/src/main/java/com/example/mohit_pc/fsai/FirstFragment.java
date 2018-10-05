6ff341a8c0f2343d725ec840d813821e6968bcc6
mohit_badve <mohitbadve@gmail.com>
2018-10-05 20:29:00 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/mohit_pc/fsai/FirstFragment.java
@@ -0,0 +1,33 @@
+package com.example.mohit_pc.fsai;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstFragment extends Fragment {
+    View myView;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.first_layout,container,false);
+        return myView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button b = view.findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "So True", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
