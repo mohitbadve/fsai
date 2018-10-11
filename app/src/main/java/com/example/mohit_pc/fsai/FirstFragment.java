65da75dd2f334afeafaf4e1b9ac2567b0e94c5cb
mohit_badve <mohitbadve@gmail.com>
2018-10-11 16:11:39 +0530

    Situation Activity Initial

--- app/src/main/java/com/example/mohit_pc/fsai/FirstFragment.java
+++ app/src/main/java/com/example/mohit_pc/fsai/FirstFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,12 +23,21 @@ public class FirstFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Button b = view.findViewById(R.id.button);
+        /*Button b = view.findViewById(R.id.button);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getActivity(), "So True", Toast.LENGTH_SHORT).show();
             }
+        });*/
+        CardView accident = view.findViewById(R.id.accidentCard);
+        accident.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Yes", Toast.LENGTH_SHORT).show();
+
+            }
+
         });
     }
 }
